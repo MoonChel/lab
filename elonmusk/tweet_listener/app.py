88c15d216f4a526032dc9f6e7c71bb45438a740a
MoonChel <newmoonchel@gmail.com>
2025-03-17 20:07:53 +0100

    add structlog

--- elonmusk/tweet_listener/app.py
+++ elonmusk/tweet_listener/app.py
@@ -1,7 +1,16 @@
 import requests
+import structlog
 from telethon import TelegramClient, events
-
 import settings
+
+# Configure structlog
+structlog.configure(
+    processors=[
+        structlog.processors.TimeStamper(fmt="iso"),
+        structlog.processors.JSONRenderer()
+    ]
+)
+logger = structlog.get_logger()
 
 client = TelegramClient(
     settings.TELEGRAM_SESSION_NAME,
@@ -13,12 +22,13 @@ client = TelegramClient(
 @client.on(events.NewMessage(chats=settings.TELEGRAM_CHANNEL_NAME))
 async def new_message_handler(event):
     event_json = event.to_dict()
-    print(f"New message in {settings.TELEGRAM_CHANNEL_NAME}: {event_json}")
+    logger.info("New message received", channel=settings.TELEGRAM_CHANNEL_NAME, event=event_json)
 
     # Send entire event JSON to FastAPI
-    requests.post(settings.WEBHOOK_URL, json=event_json)
+    response = requests.post(settings.WEBHOOK_URL, json=event_json)
+    logger.info("Sent event to FastAPI", status_code=response.status_code, response_text=response.text)
 
 
-if (__name__ == '__main__'):
-    print('listener is running...')
+if __name__ == '__main__':
+    logger.info("Listener is running...")
     client.run_until_disconnected()
